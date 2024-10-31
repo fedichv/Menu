f9351475435506cf25970e463aa08854923f35ce
fedichv <fedichev06@bk.ru>
2024-10-31 16:34:17 +0300

    Initial Commit

---
+++ MenuTests/MenuTests.swift
@@ -0,0 +1,17 @@
+//
+//  MenuTests.swift
+//  MenuTests
+//
+//  Created by Владимир Федичев on 31.10.2024.
+//
+
+import Testing
+@testable import Menu
+
+struct MenuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
